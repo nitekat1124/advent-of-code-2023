93837dd5d98ebaa2d6d9d7a11a3cc33ad77c0c04
nitekat1124 <nitekat1124@gmail.com>
2023-12-02 00:00:58 +0800

    Day 01, more simplify

--- solutions/day01.py
+++ solutions/day01.py
@@ -3,24 +3,9 @@ from utils.solution_base import SolutionBase
 
 class Solution(SolutionBase):
     def part1(self, data):
-        _sum = 0
-        for line in data:
-            digits = [int(i) for i in line if i.isdigit()]
-            _sum += digits[0] * 10 + digits[-1]
-        return _sum
+        return sum((digits := [int(i) for i in line if i.isdigit()])[0] * 10 + digits[-1] for line in data)
 
     def part2(self, data):
-        mappings = {
-            "one": "1",
-            "two": "2",
-            "three": "3",
-            "four": "4",
-            "five": "5",
-            "six": "6",
-            "seven": "7",
-            "eight": "8",
-            "nine": "9",
-        }
-
-        new_data = [[x if (x := "".join([v for k, v in mappings.items() if line[i:].startswith(k)])) else line[i] for i in range(len(line))] for line in data]
+        mappings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        new_data = [[x if (x := "".join([str(idx) for idx, val in enumerate(mappings, 1) if line[i:].startswith(val)])) else line[i] for i in range(len(line))] for line in data]
         return self.part1(new_data)
