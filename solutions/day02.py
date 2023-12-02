36fef2eefa0d77ebe8cc254c5cdd78b3a177ccf3
nitekat1124 <nitekat1124@gmail.com>
2023-12-02 22:17:10 +0800

    Day 02, add one-liner

--- solutions/day02.py
+++ solutions/day02.py
@@ -13,6 +13,11 @@ class Solution(SolutionBase):
             if all(int(cube_nums) <= thresholds[cube_color] for cube_nums, cube_color in groups):
                 possibles += int(game_info.split(" ")[1])
 
+        """
+        # one-liner
+        possibles = sum(i * all(int(cube_nums) <= thresholds[cube_color] for cube_nums, cube_color in map(str.split, line.split(": ")[1].replace(";", ",").split(", "))) for i, line in enumerate(data, 1))
+        """
+
         return possibles
 
     def part2(self, data):
@@ -28,5 +33,10 @@ class Solution(SolutionBase):
                 _set = {k: int(v) for v, k in map(str.split, _set.split(", "))}
                 counts = {k: max(v, _set.get(k, 0)) for k, v in counts.items()}
             _sum += math.prod(counts.values())
+
+        """
+        # one-liner
+        _sum = sum(math.prod([max(map(lambda s: s.get(color, 0), [{k: int(v) for v, k in map(str.split, _set.split(", "))} for _set in line.split(": ")[1].split("; ")])) for color in ("red", "green", "blue")]) for line in data)
+        """
 
         return _sum
