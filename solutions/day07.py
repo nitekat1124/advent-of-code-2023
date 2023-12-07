38c074e43e87b6b31148071c144ca829538b01a0
nitekat1124 <nitekat1124@gmail.com>
2023-12-07 14:46:32 +0800

    Day 07, change get_score's return

--- solutions/day07.py
+++ solutions/day07.py
@@ -4,13 +4,13 @@ from utils.solution_base import SolutionBase
 class Solution(SolutionBase):
     def part1(self, data):
         hands = [line.split() for line in data]
-        hands = [(self.label_to_number(hand[0]), int(hand[1]), self.get_order(hand[0])) for hand in hands]
+        hands = [(self.label_to_number(hand[0]), int(hand[1]), self.get_score(hand[0])) for hand in hands]
         hands = sorted(hands, key=lambda hand: (hand[2], hand[0]))
         return sum(rank * hand[1] for rank, hand in enumerate(hands, 1))
 
     def part2(self, data):
         hands = [line.split() for line in data]
-        hands = [(self.label_to_number(hand[0], wildcard=True), int(hand[1]), self.get_order(hand[0], wildcard=True)) for hand in hands]
+        hands = [(self.label_to_number(hand[0], wildcard=True), int(hand[1]), self.get_score(hand[0], wildcard=True)) for hand in hands]
         hands = sorted(hands, key=lambda hand: (hand[2], hand[0]))
         return sum(rank * hand[1] for rank, hand in enumerate(hands, 1))
 
@@ -25,15 +25,15 @@ class Solution(SolutionBase):
 
         return [int(mapping.get(i, i)) for i in cards]
 
-    def get_order(self, cards, wildcard=False):
-        orders = {
-            5: (6, "5"),
-            4: (5, "4"),
-            32: (4, "fh"),
-            3: (3, "3"),
-            22: (2, "2p"),
-            2: (1, "1p"),
-            1: (0, "h"),
+    def get_score(self, cards, wildcard=False):
+        types = {
+            50: "Five of a kind",
+            40: "Four of a kind",
+            32: "Full house",
+            30: "Three of a kind",
+            22: "Two pair",
+            20: "One pair",
+            10: "High card",
         }
         counter = {}
         cards = list(cards)
@@ -47,16 +47,20 @@ class Solution(SolutionBase):
             counter[card] = cards.count(card)
 
         if 5 in counter.values() or jokers == 5:
-            return orders[5]
+            rank_score = 50
         elif 4 in counter.values():
-            return orders[4 + jokers]
+            rank_score = 10 * (4 + jokers)
         elif 3 in counter.values() and 2 in counter.values():
-            return orders[32]
+            rank_score = 32
         elif 3 in counter.values():
-            return orders[3 + jokers]
+            rank_score = 10 * (3 + jokers)
         elif 2 in counter.values() and list(counter.values()).count(2) == 2:
-            return orders[22 + jokers * 10]
+            rank_score = 22 + 10 * jokers
         elif 2 in counter.values():
-            return orders[2 + jokers]
+            rank_score = 10 * (2 + jokers)
         else:
-            return orders[1 + jokers]
+            rank_score = 10 * (1 + jokers)
+
+        # using rank_score as the part of the key to sort the hands
+        # and types[rank_score] for the debug purpose
+        return rank_score, types[rank_score]
