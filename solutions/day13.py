5c79c529c8541988532e8db69b97c169883a13ea
nitekat1124 <nitekat1124@gmail.com>
2023-12-13 14:39:09 +0800

    Day 13, rewrite part 2, check the differences between patterns

--- solutions/day13.py
+++ solutions/day13.py
@@ -6,12 +6,42 @@ class Solution(SolutionBase):
         maps = ("\n".join(data)).split("\n\n")
         return sum(self.find_mirror(_map) for _map in maps)
 
+    """
+    rewrite my part2
+    thoughts: if exactly one mirror is smudged
+    then there will be exactly one difference between the two patterns
+    """
+
     def part2(self, data):
+        maps = ("\n".join(data)).split("\n\n")
+        return sum(self.find_mirror(_map, diff=1) for _map in maps)
+
+    def find_mirror(self, _map, diff=0):
+        _map_h = _map.split("\n")
+        _map_v = ["".join(c) for c in zip(*_map_h)]
+
+        for pattern, weight in ((_map_h, 100), (_map_v, 1)):
+            for i in range(1, len(pattern)):
+                a, b = pattern[:i], pattern[i:]
+                a = "".join([x for x in a[::-1]])
+                b = "".join(b)
+                if sum(x != y for x, y in zip(a, b)) == diff:
+                    return i * weight
+
+        return -1
+
+    """
+    original approach, a straight forward brute force way to find it
+    have to find the original reflection position first
+    then replace symbol one by one and find the new reflection position
+    """
+
+    def part2_org(self, data):
         maps = ("\n".join(data)).split("\n\n")
         _sum = 0
 
         for _map in maps:
-            ref_org = self.find_mirror(_map)
+            ref_org = self.find_mirror_org(_map)
             width = len(_map.split("\n")[0])
 
             s = "#."
@@ -23,7 +53,7 @@ class Solution(SolutionBase):
                 pattern[i] = pattern[i][:j] + s[(s.index(pattern[i][j]) + 1) % 2] + pattern[i][j + 1 :]
                 pattern = "\n".join(pattern)
 
-                ref = self.find_mirror(pattern, ref_org)
+                ref = self.find_mirror_org(pattern, ref_org)
                 if ref > -1:
                     _sum += ref
                     done = True
@@ -31,7 +61,7 @@ class Solution(SolutionBase):
 
         return _sum
 
-    def find_mirror(self, _map, ref_org=None):
+    def find_mirror_org(self, _map, ref_org=None):
         _map_h = _map.split("\n")
         _map_v = ["".join(c) for c in zip(*_map_h)]
 
